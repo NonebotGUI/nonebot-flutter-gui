0408a1a8e356dfe870dcd1a5174cc2df7090c03b
XTxiaoting14332 <HT30332@126.com>
2024-05-31 13:37:19 +0800

    0.1.7-fix1

--- lib/ui/settings/about.dart
+++ lib/ui/settings/about.dart
@@ -107,7 +107,7 @@ class _MoreState extends State<About> {
                   child: Align(
                 alignment: Alignment.centerRight,
                 child: InkWell(
-                  child: const Text('0.1.7'),
+                  child: const Text('0.1.7.1'),
                   onTap: () {
                     if (showImage) {
                       showDialog(
