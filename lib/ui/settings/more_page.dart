d92dc20fe7e6e8dc4ba21c31e9f8ce02fed865a1
XTxiaoting14332 <HT30332@126.com>
2024-06-05 20:21:06 +0800

    新增检查更新按钮

--- lib/ui/settings/more_page.dart
+++ lib/ui/settings/more_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:NonebotGUI/ui/settings/about.dart';
 import 'package:NonebotGUI/ui/settings/setting.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 //这段不知道干嘛用的 如果是Test应该放到 /test 文件夹
 // void main() {
@@ -18,6 +21,61 @@ class More extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<More> {
+  final String version = 'v0.1.8';
+  Future<void> check() async{
+        try {
+          final response = await http.get(Uri.parse('https://api.github.com/repos/NonebotGUI/nonebot-flutter-gui/releases/latest'));
+          if (response.statusCode == 200) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('发现新版本！'),
+                duration: Duration(seconds: 3),
+              ));
+              final jsonData = jsonDecode(response.body);
+              final tagName = jsonData['tag_name']; 
+              final changeLog = jsonData['body'];
+              final url = jsonData['html_url'];
+              if (tagName != version){
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('有新的版本：$tagName'),
+                    content: Text(changeLog),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('复制url'),
+                        onPressed: (){
+                          Clipboard.setData(ClipboardData(text: url));
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                            content: Text('已复制到剪贴板'),
+                            duration: Duration(seconds: 3),
+                          ));
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('确定'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+              }
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('检查更新失败（${response.statusCode}）'),
+                duration: const Duration(seconds: 3),
+              ));
+            }
+          } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('错误：$e'),
+                duration: const Duration(seconds: 3),
+              ));
+          }
+  }
 
   final myController = TextEditingController();
 
@@ -84,9 +142,23 @@ class _HomeScreenState extends State<More> {
                         return LicensePage(
                         applicationIcon: Image.asset('lib/assets/logo.png'),
                         applicationName: 'NonebotGUI',
-                        applicationVersion: '0.1.7',
+                        applicationVersion: '0.1.8',
                         );
                       }));
+                    })),
+            SizedBox(
+                height: 80,
+                child: InkWell(
+                    child: const Card(
+                      child: Row(
+                        children: <Widget>[
+                          Text('  检查更新'),
+                          Icon(Icons.update_rounded)
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      check();
                     })),
           ],
         ),
