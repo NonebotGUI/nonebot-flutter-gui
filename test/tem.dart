f051c4a3abb947317ec47a8e6930874d90488405
XTxiaoting14332 <HT30332@126.com>
2024-07-24 10:59:03 +0800

    修改模板文件

--- test/tem.dart
+++ test/tem.dart
@@ -1,4 +1,6 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 // 这段无意义的main不知道有什么用Test应该放到 /test 进行
 // 如果只是检查单页状态，可以在DEBUG Console里面调用NavPush来跳转路由
@@ -33,15 +35,52 @@ class _HomeScreenState extends State<Template> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "1",
-          style: TextStyle(color: Colors.white),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: Row(
+          children: [
+            Expanded(
+              child: MoveWindow(
+                child: AppBar(
+                  title: Text("data"),
+                  actions: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.remove_rounded),
+                      color: Colors.white,
+                      onPressed: () => appWindow.minimize(),
+                      iconSize: 20,
+                      tooltip: "最小化",
+                    ),
+                    appWindow.isMaximized ?
+                      IconButton(
+                        icon: const Icon(Icons.rectangle_outlined),
+                        color: Colors.white,
+                        onPressed: () => appWindow.restore(),
+                        iconSize: 20,
+                        tooltip: "恢复大小",
+                      ) :
+                    IconButton(
+                        icon: const Icon(Icons.rectangle_outlined),
+                        color: Colors.white,
+                        onPressed: () => appWindow.maximize(),
+                        iconSize: 20,
+                        tooltip: "最大化",
+                      ),
+                    IconButton(
+                      icon: const Icon(Icons.close_rounded),
+                      color: Colors.white,
+                      onPressed: () => windowManager.hide(),
+                      iconSize: 20,
+                      tooltip: "关闭",
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
-        actions: const <Widget>[
-        ],
-        backgroundColor: const Color.fromRGBO(238, 109, 109, 1),
       ),
+        backgroundColor: const Color.fromRGBO(238, 109, 109, 1),
     );
   }
 }
