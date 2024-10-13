48f8779d7ac838a4da1691d75967da14fc8ed2f1
XTxiaoting14332 <HT30332@126.com>
2024-10-13 10:48:00 +0800

    修复换行导致列表爆炸的bug

--- lib/ui/manage/env.dart
+++ lib/ui/manage/env.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:NoneBotGUI/utils/manage.dart';
 import 'package:NoneBotGUI/utils/userConfig.dart';
 import 'package:flutter/material.dart';
@@ -13,9 +12,11 @@ class EditEnv extends StatefulWidget {
 
 class _HomeScreenState extends State<EditEnv> {
   int _selectedIndex = 0;
-  List<String> _envList = ['.env', '.env.prod', '.env.dev'];
+  final List<String> _envList = ['.env', '.env.prod', '.env.dev'];
   List<String> _envContent = [];
+  RegExp pattern = RegExp(r'^[^=]+=[^=]+$');
   String envFile = '.env';
+  final TextEditingController _fileContentController = TextEditingController();
 
   void _onItemTapped(int index) {
     setState(() {
@@ -33,6 +34,7 @@ class _HomeScreenState extends State<EditEnv> {
 
   @override
   void dispose() {
+    _fileContentController.dispose();
     super.dispose();
   }
 
@@ -111,105 +113,158 @@ class _HomeScreenState extends State<EditEnv> {
                 color: Colors.grey,
               ),
               File('${Bot.path()}/$envFile').existsSync()
-              ? ListView.builder(
-                itemCount: _envContent.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) => ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            _envContent[index].split('=')[0],
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            _envContent[index].split('=')[1],
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            IconButton(
-                              icon: const Icon(Icons.edit),
-                              tooltip: '编辑',
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    final TextEditingController _controller = TextEditingController();
-                                    _controller.text = _envContent[index].split('=')[1];
-                                    return AlertDialog(
-                                      title: const Text('修改变量值'),
-                                      content: TextField(
-                                        controller: _controller,
-                                        decoration: const InputDecoration(
-                                          hintText: '请输入新的值',
-                                        ),
-                                      ),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _envContent
+                          .where((line) => pattern.hasMatch(line))
+                          .length,
+                      itemBuilder: (context, index) {
+                        String filteredLine = _envContent
+                            .where((line) => pattern.hasMatch(line))
+                            .toList()[index];
+                        List<String> parts = filteredLine.split('=');
+                        String variableName = parts[0];
+                        String variableValue = parts.length > 1 ? parts[1] : '';
+
+                        return ListTile(
+                          title: Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Center(
+                                  child: Text(
+                                    variableName,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Center(
+                                  child: Text(
+                                    variableValue,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    IconButton(
+                                      icon: const Icon(Icons.edit),
+                                      tooltip: '编辑',
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            final TextEditingController
+                                                _controller1 =
+                                                TextEditingController(
+                                                    text: variableName);
+                                            final TextEditingController
+                                                _controller2 =
+                                                TextEditingController(
+                                                    text: variableValue);
+                                            return AlertDialog(
+                                              title: const Text('编辑变量'),
+                                              content: SizedBox(
+                                                height: 150,
+                                                child: Column(
+                                                  children: <Widget>[
+                                                    TextField(
+                                                      controller: _controller1,
+                                                      decoration:
+                                                          const InputDecoration(
+                                                        hintText: '请输入变量名',
+                                                        labelText: '变量名',
+                                                      ),
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                    TextField(
+                                                      controller: _controller2,
+                                                      decoration:
+                                                          const InputDecoration(
+                                                        hintText: '请输入变量值',
+                                                        labelText: '变量值',
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              actions: <Widget>[
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  child: const Text('取消'),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () {
+                                                    _envContent[
+                                                            _envContent.indexOf(
+                                                                filteredLine)] =
+                                                        '${_controller1.text}=${_controller2.text}';
+                                                    File('${Bot.path()}/$envFile')
+                                                        .writeAsStringSync(
+                                                            _envContent
+                                                                .join('\n'));
+                                                    Navigator.of(context).pop();
+                                                    _loadEnv();
+                                                  },
+                                                  child: const Text('确定'),
+                                                ),
+                                              ],
+                                            );
                                           },
-                                          child: const Text('取消'),
-                                        ),
-                                        TextButton(
-                                          onPressed: () {
-                                            _envContent[index] = '${_envContent[index].split('=')[0]}=${_controller.text}';
-                                            File('${Bot.path()}/$envFile').writeAsStringSync(_envContent.join('\n'));
-                                            Navigator.of(context).pop();
-                                            _loadEnv();
-                                          },
-                                          child: const Text('确定'),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete),
-                              tooltip: '删除',
-                              onPressed: () {
-                                _envContent.removeAt(index);
-                                File('${Bot.path()}/$envFile').writeAsStringSync(_envContent.join('\n'));
-                                _loadEnv();
-                              },
-                            )
-                          ],
-                        )
-                      )
-                    ],
-                  ),
-                ),
-              )
-              : Center(
-                child: Column(
-                  children: <Widget>[
-                    const Text('文件不存在'),
-                    Image.asset('lib/assets/loading.gif')
-                  ],
-                ),
-              ),
+                                        );
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.delete),
+                                      tooltip: '删除',
+                                      onPressed: () {
+                                        _envContent.remove(filteredLine);
+                                        File('${Bot.path()}/$envFile')
+                                            .writeAsStringSync(
+                                                _envContent.join('\n'));
+                                        _loadEnv();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Column(
+                        children: <Widget>[
+                          const Text('文件不存在'),
+                          Image.asset('lib/assets/loading.gif')
+                        ],
+                      ),
+                    ),
             ],
           ))),
-          floatingActionButton: FloatingActionButton(
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: 'add_var',
+            tooltip: '添加变量',
             shape: const CircleBorder(),
             onPressed: () {
               showDialog(
                 context: context,
                 builder: (BuildContext context) {
-                  final TextEditingController _controller1 = TextEditingController();
-                  final TextEditingController _controller2 = TextEditingController();
+                  final TextEditingController _varNameController =
+                      TextEditingController();
+                  final TextEditingController _varValueController =
+                      TextEditingController();
                   return AlertDialog(
                     title: const Text('添加变量'),
                     content: SizedBox(
@@ -217,7 +272,7 @@ class _HomeScreenState extends State<EditEnv> {
                       child: Column(
                         children: <Widget>[
                           TextField(
-                            controller: _controller1,
+                            controller: _varNameController,
                             decoration: const InputDecoration(
                               hintText: '请输入变量名',
                               labelText: '变量名',
@@ -227,7 +282,7 @@ class _HomeScreenState extends State<EditEnv> {
                             height: 10,
                           ),
                           TextField(
-                            controller: _controller2,
+                            controller: _varValueController,
                             decoration: const InputDecoration(
                               hintText: '请输入变量值',
                               labelText: '变量值',
@@ -245,10 +300,12 @@ class _HomeScreenState extends State<EditEnv> {
                       ),
                       TextButton(
                         onPressed: () {
-                          _envContent.add('${_controller1.text}=${_controller2.text}');
-                          File('${Bot.path()}/$envFile').writeAsStringSync(_envContent.join('\n'));
-                          Navigator.of(context).pop();
+                          _envContent.add(
+                              '${_varNameController.text}=${_varValueController.text}');
+                          File('${Bot.path()}/$envFile')
+                              .writeAsStringSync(_envContent.join('\n'));
                           _loadEnv();
+                          Navigator.of(context).pop();
                         },
                         child: const Text('确定'),
                       ),
@@ -259,13 +316,71 @@ class _HomeScreenState extends State<EditEnv> {
             },
             child: const Icon(Icons.add),
           ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            heroTag: 'edit_file',
+            tooltip: '编辑文件',
+            shape: const CircleBorder(),
+            onPressed: () {
+              _fileContentController.text = _envContent.join('\n');
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('编辑 $envFile 文件内容'),
+                    content: SizedBox(
+                      width: double.maxFinite,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _fileContentController,
+                              maxLines: null,
+                              expands: true,
+                              textAlignVertical: TextAlignVertical.top,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: '编辑文件内容',
+                                alignLabelWithHint: true,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('取消'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          File('${Bot.path()}/$envFile')
+                              .writeAsStringSync(_fileContentController.text);
+                          _loadEnv();
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('保存'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Icon(Icons.edit),
+          ),
+        ],
+      ),
     );
   }
 
   _loadEnv() {
     File('${Bot.path()}/$envFile').readAsString().then((String contents) {
       setState(() {
-        _envContent = contents.split('\n').where((line) => line.isNotEmpty).toList();
+        _envContent = contents.split('\n').toList();
       });
     });
   }
